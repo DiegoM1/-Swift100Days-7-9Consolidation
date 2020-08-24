8106677e1e306bf017df83cf8edc5f0b2d9e2522
diego.monteagudo <diego.monteagudo@fisglobal.com>
2020-08-24 16:05:11 -0500

    Initial Commit

---
+++ project 7-9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project 7-9
+//
+//  Created by Diego Sebastián Monteagudo Díaz on 8/24/20.
+//  Copyright © 2020 Diego Sebastián Monteagudo Díaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
